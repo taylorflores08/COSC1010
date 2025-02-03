40950aa4c27169243b16587e7efdcd7ca0fef6bc
taylorflores08 <taylorflores@westernwyoming.edu>
2025-02-02 21:09:25 -0700

    Update main.py

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Taylor Flores
+# 2/2/25
 # Sales Prediction Programming Project
 # COSC 1010
 #
@@ -8,7 +8,8 @@
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
+total_sales = float (input ('Enter the projected sales: '))
 # Calculate the projected profit.
-
+profit + total_sales * 0.23
 # Print the projected profit.
+print ('The profit is $', format (profit, ',.2f'))
